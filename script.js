1aea693214f4bd0d64d081b5316c89118c4957d8
Glaskario <shvavzhjnsbsjsjsjsj@gmail.com>
2023-04-19 10:00:45 -0300

    Adicionando mais alguma funcionalidades ao site

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrolly > 20){
+        if(this.scrollTop() > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
